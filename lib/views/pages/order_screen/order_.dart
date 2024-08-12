d55e0351425e3eff808a79c0a71a98c10d044164
vishal-jaat-007 <vjaat5597@gmail.com>
2024-08-12 10:41:26 +0530

    changes

--- lib/views/pages/order_screen/order_.dart
+++ lib/views/pages/order_screen/order_.dart
@@ -6,7 +6,6 @@ import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:tiffin_service_customer/resources/config/routes/app_routes.dart';
 import 'package:tiffin_service_customer/resources/i18n/translation_files.dart';
-import 'package:tiffin_service_customer/resources/primary_decorations.dart';
 import 'package:tiffin_service_customer/singletonClasses/singleton.dart';
 import 'package:tiffin_service_customer/view_model/controllers/Theme%20Controller/theme_controller.dart';
 import 'package:tiffin_service_customer/view_model/controllers/add_on_controller.dart';
