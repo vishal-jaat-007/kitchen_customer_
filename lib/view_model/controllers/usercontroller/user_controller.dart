151315cf56887ee0eea70fc32bd7ed1f33866c2a
vishal-jaat-007 <vjaat5597@gmail.com>
2024-08-13 14:02:43 +0530

    changes

--- lib/view_model/controllers/usercontroller/user_controller.dart
+++ lib/view_model/controllers/usercontroller/user_controller.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
 
 class UserController extends GetxController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
